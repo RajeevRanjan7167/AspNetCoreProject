013af76ca74118d4738178681c02def053cdbbde
unknown <rajeev.ranjan8284@gmail.com>
2020-04-18 01:31:45 +0530

    Yes

--- AspNetCoreApp-SPA/src/app/nav/nav.component.ts
+++ AspNetCoreApp-SPA/src/app/nav/nav.component.ts
@@ -1,38 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_Services/Auth.service';
+import { AlertifyService } from '../_Services/alertify.service';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
-export class NavComponent implements OnInit {  
+export class NavComponent implements OnInit {
   model: any = {};
-  
-  constructor(private authService: AuthService) {  }
 
-  ngOnInit() {
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
+
+  ngOnInit() {}
+
+  login() {
+    this.authService.login(this.model).subscribe(
+      (next) => {
+        this.alertify.success('Logged in successfully !!!');
+      },
+      (error) => {
+        this.alertify.error('Failed to login !!!');
+      }
+    );
   }
 
-  login()
-  {
-    this.authService.login(this.model).subscribe(next => {
-      console.log('Logged in successfully !!!');
-    }, error => {
-      console.log('Failed to login !!!');
-    });
+  loggedIn() {
+    return this.authService.loggedIn();
   }
 
-  loggedIn()
-  {
-    const token = localStorage.getItem('token');
-    return !! token;
-  }
-
-  logout()
-  {
+  logout() {
     localStorage.removeItem('token');
-    console.log('logged out !!!');
+    this.alertify.message('logged out !!!');
   }
-
 }
