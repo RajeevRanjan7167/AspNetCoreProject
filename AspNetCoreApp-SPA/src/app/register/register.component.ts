abd97408db3c1f860f1164c8bd9a702eb130cb41
unknown <rajeev.ranjan8284@gmail.com>
2020-04-17 13:43:51 +0530

    End of registration Module

---
+++ AspNetCoreApp-SPA/src/app/register/register.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AuthService } from '../_Services/Auth.service';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  @Input() valuesFormHome: any;
+  @Output() cancelRegister = new EventEmitter();
+
+  model: any = {};
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+  }
+
+  register()
+  {
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration successfull');
+    }, error => {
+      console.log(error);
+    });
+  }
+
+  cancel()
+  {
+    this.cancelRegister.emit(false);
+    console.log('Cancel');
+  }
+
+}
